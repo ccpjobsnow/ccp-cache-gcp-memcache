1befabcaad8d40967cc8b83cbd0c900b8c032251
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-12-03 22:10:35 -0300

    .

--- src/main/java/com/ccp/implementations/cache/gcp/memcache/GcpMemCache.java
+++ src/main/java/com/ccp/implementations/cache/gcp/memcache/GcpMemCache.java
@@ -2,7 +2,6 @@ package com.ccp.implementations.cache.gcp.memcache;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.function.Function;
 
 import com.ccp.decorators.CcpJsonRepresentation;
 import com.ccp.especifications.cache.CcpCache;
@@ -11,6 +10,7 @@ import com.google.appengine.api.memcache.MemcacheService;
 import com.google.appengine.api.memcache.MemcacheServiceFactory;
 
 class GcpMemCache implements CcpCache {
+	
 	private static MemcacheService memcacheService = MemcacheServiceFactory.getMemcacheService();
 
 	@SuppressWarnings("unchecked")
@@ -29,51 +29,8 @@ class GcpMemCache implements CcpCache {
 		CcpJsonRepresentation jr = new CcpJsonRepresentation(map);
 		return jr;
 	}
-	@SuppressWarnings("unchecked")
-	public <V> V get(String key, CcpJsonRepresentation json, Function<CcpJsonRepresentation, V> taskToGetValue, int cacheSeconds) {
 
-		Object object = this.get(key);
 
-		if (object != null) {
-			return (V) object;
-		}
-		V value = taskToGetValue.apply(json);
-		this.put(key, value, cacheSeconds);
-
-		return value;
-	}
-
-	@SuppressWarnings("unchecked")
-	
-	public <V> V getOrDefault(String key, V defaultValue) {
-		Object object = this.get(key);
-		
-		if(object == null) {
-			return defaultValue;
-		}
-		return (V) object;
-		
-	}
-
-	@SuppressWarnings("unchecked")
-	
-	public <V> V getOrThrowException(String key, RuntimeException e) {
-		Object object = this.get(key);
-		
-		if(object == null) {
-			throw e;
-		}
-		
-		return (V) object;
-	}
-
-	
-	public boolean isPresent(String key) {
-		boolean isPresent = this.get(key) != null;
-		return isPresent;
-	}
-
-	
 	public void put(String key, Object value, int secondsDelay) {
 		Expiration arg2 = Expiration.byDeltaSeconds(secondsDelay);
 		if(value instanceof CcpJsonRepresentation) {
